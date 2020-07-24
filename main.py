622e060b87e51845f32fb91b03aefff5bc11455b
marcus2x <wowmarc11@gmail.com>
2020-07-24 17:19:33 +0000

    nested loops

---
+++ main.py
@@ -0,0 +1,9 @@
+for i in range(1):
+ for j in range(6):
+  print("outter loop ",(i,j))
+for i in range(2):
+  for j in range(6):
+    print("inner loop ",(i,j))
+    for i in range(1):
+      for j in range(10):
+    print ("outter loop")    
